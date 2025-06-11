b613b340ae6baf4a5bcf5e11c2e29e158125f285
Goutham Singh <goutham.singh@wisseninfotech.com>
2025-06-11 11:34:35 +0530

    deploy 2

--- aifunc_site/settings.py
+++ aifunc_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['multigenai.onrender.com']
+ALLOWED_HOSTS = ['multi-genai.onrender.com']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Application definition
